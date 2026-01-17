29e41272e191f6ae96bc1fa5b90463fecb187a56
Nishant Holla <nishantholla19@gmail.com>
2026-01-17 22:42:01 +0530

    inverted_index: Refactor

--- 01.inverted_index/__init__.py
+++ 01.inverted_index/__init__.py
@@ -1,3 +1,4 @@
 from .inverted_index import InvertedIndex
 from .list_node import ListNode
 from .usage import print_usage
+from .utils import index_file, database_file, read_dataset, get_query_from_user
--- 01.inverted_index/main.py
+++ 01.inverted_index/main.py
@@ -1,47 +1,148 @@
 import sys
 import random
-from inverted_index import InvertedIndex, T_document
+from inverted_index import InvertedIndex
 from pathlib import Path
 from usage import print_usage
 import time
+from utils import index_file, database_file, read_dataset, get_query_from_user
 
 
-def index_file(d: Path | str) -> Path:
-    return Path(d) / "inverted_index.json"
+def index(dataset_file: Path | str, output_dir: Path | str) -> int:
+    if not Path(dataset_file).is_file():
+        return 1
+
+    if not Path(output_dir).is_dir():
+        return 2
+
+    documents = read_dataset(dataset_file)
+    i_file = index_file(output_dir)
+    d_file = database_file(output_dir)
+
+    ir.index(documents)
+    ir.save(i_file, d_file)
+
+    return 0
 
 
-def database_file(d: Path | str) -> Path:
-    return Path(d) / "database.json"
+def query(index_dir: Path | str) -> int:
+    if not Path(index_dir).is_dir():
+        return 1
+
+    idx_file = index_file(index_dir)
+    db_file = database_file(index_dir)
+
+    ir.load(idx_file, db_file)
+    op, a, b = get_query_from_user()
+
+    result = ir.query(op, a, b)
+    for r in result:
+        print(f"{r['name']}\n{r['review']}\n\n")
+
+    print(f"Found {len(result)} results")
+    return 0
 
 
-def read_dataset(dataset_file: Path | str) -> list[T_document]:
-    with open(dataset_file, "r", encoding="latin-1") as f:
-        data = f.readlines()
+def test(test_file: Path | str, dataset_file: Path | str) -> int:
+    if not Path(test_file).is_file():
+        return 1
 
-    documents = []
-    for line in data:
-        movie_name, review = line.split("\t")
-        documents.append({"name": movie_name.strip(), "review": review.strip()})
+    with open(test_file, "r", encoding="latin-1") as f:
+        tests = f.readlines()
 
-    return documents
+    if not Path(dataset_file).is_file():
+        return 2
+
+    documents = read_dataset(dataset_file)
+
+    index_start = time.perf_counter()
+    ir.index(documents)
+    index_end = time.perf_counter()
+    index_time = index_end - index_start
+
+    query_time = 0
+    operations = {
+        "and": {"time": 0.0, "count": 0},
+        "or": {"time": 0.0, "count": 0},
+        "not": {"time": 0.0, "count": 0},
+    }
+
+    for test in tests:
+        test = test.strip().split(" ")
+
+        if len(test) == 2:
+            a, o = test
+            b = None
+        else:
+            a, o, b = test
+
+        query_start = time.perf_counter()
+        ir.query(o, a, b)
+        query_end = time.perf_counter()
+        query_time += query_end - query_start
+
+        operations[o]["time"] += query_end - query_start
+        operations[o]["count"] += 1
+
+    print(
+        f"Index time: {index_time:.8f}s for {len(documents)} documents"
+        f" => {index_time / len(documents):.8f}s per doc"
+    )
+
+    print(
+        f"Query time: {query_time:.8f}s for {len(tests)} tests"
+        f" => {query_time / len(tests):.8f}s per test"
+    )
+    for o, v in operations.items():
+        print(
+            f"     {o} operation: {v['time']:.8f}s for {v['count']} operations"
+            f" => {v['time'] / v['count']:.8f}s per operation"
+        )
+    print()
+
+    return 0
 
 
-def get_query_from_user() -> tuple[str, str, str | None]:
-    while True:
-        op = input("Enter operation: ")
-        if op not in ["and", "not", "or"]:
-            print("Invalid operation")
-            continue
+def generate_test(index_dir: Path | str, count: int, test_file: Path | str) -> int:
+    if not Path(index_dir).is_dir():
+        return 1
 
-        break
+    if not Path(test_file).parent.is_dir():
+        return 2
 
-    a = input("Enter first term: ")
-    if op != "not":
-        b = input("Enter second term: ")
-    else:
-        b = None
+    i_file = index_file(index_dir)
+    d_file = database_file(index_dir)
 
-    return (op, a, b)
+    ir.load(i_file, d_file)
+    vocab = list(ir.get_vocab())
+    operations = ["and", "or", "not"]
+
+    tests = []
+
+    for _ in range(count):
+        o = random.choice(operations)
+        a = random.choice(vocab)
+        if o != "not":
+            b = random.choice(vocab)
+        else:
+            b = ""
+        tests.append(f"{a} {o} {b}")
+
+    with open(test_file, "w", encoding="latin-1") as f:
+        f.write("\n".join(tests))
+
+    return 0
+
+
+def test_tokenizer(dataset_file: Path | str) -> int:
+    if not Path(dataset_file).is_file():
+        return 1
+
+    documents = read_dataset(dataset_file)
+    print(documents[0]["review"])
+    print()
+    print(ir._tokenize(documents[0]["review"]))
+
+    return 0
 
 
 if __name__ == "__main__":
@@ -57,42 +158,29 @@ if __name__ == "__main__":
             print_usage("index")
             exit(1)
 
-        dataset_file = Path(sys.argv[2])
-        if not dataset_file.is_file():
+        dataset_file = sys.argv[2]
+        output_dir = sys.argv[3]
+        ec: int = index(dataset_file, output_dir)
+
+        if ec == 1:
             print(f"Failed to find file {dataset_file}")
-            exit(2)
+            exit(ec)
 
-        output_dir = Path(sys.argv[3])
-        if not output_dir.is_dir():
+        elif ec == 2:
             print(f"Failed to find directory {output_dir}")
-
-        documents = read_dataset(dataset_file)
-        i_file = index_file(output_dir)
-        d_file = database_file(output_dir)
-
-        ir.index(documents)
-        ir.save(i_file, d_file)
+            exit(ec)
 
     elif sys.argv[1] == "query":
         if len(sys.argv) != 3:
             print_usage("query")
             exit(1)
 
-        i_dir = Path(sys.argv[2])
-        if not i_dir.is_dir():
-            print(f"Failed to find directory {i_dir}")
+        index_dir = Path(sys.argv[2])
+        ec = query(index_dir)
 
-        i_file = index_file(i_dir)
-        d_file = database_file(i_dir)
-
-        ir.load(i_file, d_file)
-        op, a, b = get_query_from_user()
-
-        result = ir.query(op, a, b)
-        for r in result:
-            print(f"{r['name']}\n{r['review']}\n\n")
-
-        print(f"Found {len(result)} results")
+        if ec == 1:
+            print(f"Failed to find directory {index_dir}")
+            exit(ec)
 
     elif sys.argv[1] == "time":
         if len(sys.argv) != 4:
@@ -100,73 +188,23 @@ if __name__ == "__main__":
             exit(1)
 
         test_file = Path(sys.argv[2])
-        if not test_file.is_file():
-            print(f"Failed to find file {test_file}")
-            exit(2)
-
-        with open(test_file, "r", encoding="latin-1") as f:
-            tests = f.readlines()
-
         dataset_file = Path(sys.argv[3])
-        if not dataset_file.is_file():
+        ec = test(test_file, dataset_file)
+
+        if ec == 1:
+            print(f"Failed to find file {test_file}")
+            exit(ec)
+
+        elif ec == 2:
             print(f"Failed to find file {dataset_file}")
-            exit(2)
-
-        documents = read_dataset(dataset_file)
-
-        index_start = time.perf_counter()
-        ir.index(documents)
-        index_end = time.perf_counter()
-        index_time = index_end - index_start
-
-        query_time = 0
-        operations = {
-            "and": {"time": 0.0, "count": 0},
-            "or": {"time": 0.0, "count": 0},
-            "not": {"time": 0.0, "count": 0},
-        }
-
-        for i, test in enumerate(tests):
-            test = test.strip().split(" ")
-
-            if len(test) == 2:
-                a, o = test
-                b = None
-            else:
-                a, o, b = test
-
-            query_start = time.perf_counter()
-            ir.query(o, a, b)
-            query_end = time.perf_counter()
-            query_time += query_end - query_start
-
-            operations[o]["time"] += query_end - query_start
-            operations[o]["count"] += 1
-
-        print(
-            f"Index time: {index_time:.8f}s for {len(documents)} documents"
-            f" => {index_time / len(documents):.8f}s per doc"
-        )
-
-        print(
-            f"Query time: {query_time:.8f}s for {len(tests)} tests"
-            f" => {query_time / len(tests):.8f}s per test"
-        )
-        for o, v in operations.items():
-            print(
-                f"     {o} operation: {v['time']:.8f}s for {v['count']} operations"
-                f" => {v['time'] / v['count']:.8f}s per operation"
-            )
+            exit(ec)
 
     elif sys.argv[1] == "generate_test":
         if len(sys.argv) != 5:
-            print_usage("time")
+            print_usage("generate_test")
             exit(1)
 
-        i_dir = Path(sys.argv[2])
-        if not i_dir.is_dir():
-            print(f"Failed to find directory {i_dir}")
-            exit(2)
+        index_dir = Path(sys.argv[2])
 
         try:
             count = int(sys.argv[3])
@@ -175,30 +213,16 @@ if __name__ == "__main__":
             exit(2)
 
         test_file = Path(sys.argv[4])
-        if not test_file.parent.is_dir():
-            print(f"Failed to find directory {test_file.parent}")
-            exit(2)
 
-        i_file = index_file(i_dir)
-        d_file = database_file(i_dir)
+        ec = generate_test(index_dir, count, test_file)
 
-        ir.load(i_file, d_file)
-        vocab = list(ir.get_vocab())
-        operations = ["and", "or", "not"]
+        if ec == 1:
+            print(f"Failed to find directory {index_dir}")
+            exit(ec)
 
-        tests = []
-
-        for _ in range(count):
-            o = random.choice(operations)
-            a = random.choice(vocab)
-            if o != "not":
-                b = random.choice(vocab)
-            else:
-                b = ""
-            tests.append(f"{a} {o} {b}")
-
-        with open(test_file, "w", encoding="latin-1") as f:
-            f.write("\n".join(tests))
+        elif ec == 2:
+            print(f"Failed to find directory {Path(test_file).parent}")
+            exit(ec)
 
     elif sys.argv[1] == "test_tokenizer":
         if len(sys.argv) != 3:
@@ -206,15 +230,12 @@ if __name__ == "__main__":
             exit(1)
 
         dataset_file = Path(sys.argv[2])
-        if not dataset_file.is_file():
+
+        ec = test_tokenizer(dataset_file)
+
+        if ec == 1:
             print(f"Failed to find file {dataset_file}")
-            exit(2)
-
-        documents = read_dataset(dataset_file)
-
-        print(documents[0]["review"])
-        print()
-        print(ir._tokenize(documents[0]["review"]))
+            exit(ec)
 
     else:
         print_usage()
